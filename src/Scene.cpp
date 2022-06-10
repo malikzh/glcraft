6cc7f9d570f2c419fe2d8c9e8250ce6043324af9
Malik Zharykov <cmalikz.h@gmail.com>
2022-06-11 04:45:37 +0600

    add comments

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -1,5 +1,10 @@
 #include "glcraft.hpp"
 
+/**
+ * The main rendering method
+ *
+ * Это главный метод для рендеринга сцены
+ */
 void Scene::render() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
