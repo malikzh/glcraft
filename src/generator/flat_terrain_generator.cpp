eb281838c27ee37bfade84a6016bc432b989476a
Malik Zharykov <cmalikz.h@gmail.com>
2022-06-28 00:56:43 +0600

    terrain generator

--- src/generator/flat_terrain_generator.cpp
+++ src/generator/flat_terrain_generator.cpp
@@ -5,7 +5,7 @@
 void terrain_generate_flat() {
     for (size_t i=0; i<100; ++i) {
         for (size_t j=0; j<100; ++j) {
-            world->setBlock((int32_t)i-1, (int32_t)( 5.0f + floor(3.0f*cosf((float)i/3.0f) + sin(3.0f*cosf((float)j/3.0f)) ) ), (int32_t)j-1, BlockType::GRASS);
+            world->setBlock((int32_t)i-1, (int32_t)( 10.0f + floor(3.0f*cosf((float)i/3.0f) + 3.0f*cosf((float)j/3.0f) ) ), (int32_t)j-1, BlockType::GRASS);
         }
     }
 }
