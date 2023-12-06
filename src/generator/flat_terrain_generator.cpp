9fb4b7cb5b31eb85ba78ab9030e5426f448b0f08
Malik Zharykov <Malik.Zharykov@tele2.kz>
2023-12-06 04:40:01 -0500

    feat: Change tree generation algorithm

--- src/generator/flat_terrain_generator.cpp
+++ src/generator/flat_terrain_generator.cpp
@@ -3,6 +3,7 @@
 #include "../glcraft.hpp"
 
 void terrain_generate_flat() {
+    long X = 8;
     for (size_t i=0; i<150; ++i) {
         for (size_t j=0; j<150; ++j) {
             int32_t y = (int32_t)( 10.0f + floor(3.0f*cosf((float)i/3.0f) + 3.0f*cosf((float)j/3.0f) ) );
@@ -10,7 +11,9 @@ void terrain_generate_flat() {
 
             world->setBlock((int32_t)i-1, y, (int32_t)j-1, BlockType::GRASS);
 
-            if (rand() % 200 == 0) {
+            X = (1103515245 * X + 12345) % 65536;
+
+            if (X  % 107 == 0) {
                 generate_tree(i, y, j);
             }
         }
